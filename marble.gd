102d2020170da37874895925dfa3a369b627f999
carlosbispo <a26633@gmail.com>
2022-06-13 18:02:56 +0100

    Improvements in rendering scene

--- marble.gd
+++ marble.gd
@@ -1,25 +1,27 @@
 extends RigidBody
 
 onready var sphere = $sphere as MeshInstance
-var texture_assets = [
-		preload("res://Assets/Marble/blueMarbleTexture.jpg"),
-		preload("res://Assets/Marble/greenMarbleTexture.jpg"),
-		preload("res://Assets/Marble/purpleMarbleTexture.jpg"),
-		preload("res://Assets/Marble/redMarbleTexture.jpg"),
+var materials = [
+		preload("res://Assets/Marble/blueMat.material"),
+		preload("res://Assets/Marble/greenMat.material"),
+		preload("res://Assets/Marble/purpleMat.material"),
+		preload("res://Assets/Marble/redMat.material"),
 ]
 
 enum TEXTURES {BLUE, GREEN, PURPLE, RED}
 export(TEXTURES) var color = 0
 
 func _ready():
+	pass
 	randomize()
 	_set_color(randi() % TEXTURES.size())
 
 func _set_color(col):
-	var material = SpatialMaterial.new()	
-	material.metallic = 0
-	material.roughness = 0.1
-	material.albedo_texture = texture_assets[col]
-	sphere.set_surface_material(0, material)
+	sphere.set_surface_material(0, materials[col])
+	#var material = SpatialMaterial.new()	
+	#material.metallic = 0
+	#material.roughness = 0.1
+	#material.albedo_texture = texture_assets[col]
+	#sphere.set_surface_material(0, material)
 	
 	
